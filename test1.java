2a71afaed2ad2281f4ca9ddef1b810df5023e308
gunhot556 <121473062+gunhot556@users.noreply.github.com>
2023-02-06 14:09:59 +0700

    Add files via upload

---
+++ test1.java
@@ -0,0 +1,26 @@
+package lib;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class test1 {
+
+    QuizOneJunit quizOneJunit = new QuizOneJunit();
+
+    @Test
+    public void testSquare() {
+        
+        assertEquals(4, quizOneJunit.square(2));
+    }
+
+    @Test
+    public void testCountLetterA() {
+        
+        assertEquals(2, quizOneJunit.countLetterA("baA"));
+    }
+
+    @Test 
+    public void testCheckTwoLetter() { 
+        
+        assertFalse(quizOneJunit.checkTwoLetter("a")); 
+    } 
+}
